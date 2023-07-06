698a9f0e0f7f4af239b5559ea53a313ca6fa9a4b
mbron770 <mbronfin.tsd@gmail.com>
2023-07-06 18:41:19 -0400

    a

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ''):
+    
+      self.value = value
+  
+  @property
+  
+  def value(self):
+    return self._value
+  
+  @value.setter
+  
+  def value(self, value): 
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print('The value must be a string.') 
+      
+  def is_sentence(self): 
+    return True if self.value[-1] == '.' else False
+  
+  def is_question(self):
+    return True if self.value[-1] == '?' else False
+  
+  def is_exclamation(self):
+    return True if self.value[-1] == '!' else False
+  
+  def count_sentences(self):
+    for ending in ['?', '!']:
+      self.value = self.value.replace(ending, '.') 
+      
+    return len([s for s in self.value.split('.') if s])
+
